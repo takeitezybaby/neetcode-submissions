1e855546cc62ed380ad1875ba2edb3c40cb9bc96
Yash Chawla <yash336aarav@gmail.com>
2026-09-07 14:49:48 +0530

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def backtrack(n, nums) :
+            if n == 0 or n==1:
+                return nums[0]
+            prev1 = nums[0]
+            prev2 = max(nums[0], nums[1])
+            for i in range(2,n) :
+                temp = prev2
+                prev2 = max(nums[i] + prev1, prev2)
+                prev1 = temp
+            return prev2
+        n = len(nums)
+        return backtrack(n,nums)
